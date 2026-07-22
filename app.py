60166f2967d32c285f5dc5903bd39eee68d7a106
Alone Monster <164857018+alone-monster@users.noreply.github.com>
2026-07-22 06:37:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import time
 import telebot
 import os
+import threading
+from flask import Flask
 from telebot import types
 from telebot.types import BotCommand
 from telebot.types import InlineQueryResultArticle, InputTextMessageContent 
@@ -383,7 +385,17 @@ def download(message):
 
 
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run_bot():
     bot.remove_webhook()
     bot.infinity_polling(skip_pending=True)
-    
+
+if __name__ == "__main__":
+    threading.Thread(target=run_bot).start()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
